8989ed7e2498626819331e385eda921db3a01759
sean2249 <sean22492249@gmail.com>
2017-05-06 02:31:50 +0800

    revise run_test.py

--- test_15/run_test.py
+++ test_15/run_test.py
@@ -1,59 +1,102 @@
-import os 
+import os
 import sys
+from datetime import datetime
 
-# System argument value 
+
+# System argument value
 #  sys_truth = sys.argv[1]
 #  test_num = sys.argv[2]
 #  rmchar = sys.argv[3]
 
 # Remove char for sub task 1
 #  if test_num==1 & rmchar==1:
-    #  print
+#  print
 
 def rmchar(sys_truth):
-    outputname = os.path.splitext(sys_truth)[0]+'_rmch.txt'
+    outputname = os.path.splitext(sys_truth)[0] + '_rmch.txt'
 
     with open(sys_truth, 'r', encoding='utf8') as fp, open(outputname, 'w') as wp:
         for line in fp:
             line = line.strip()
             sep = line.split(', ')
             #  print(sep[1::2])
-            wp.write(sep[0]+', ')
+            wp.write(sep[0] + ', ')
             wp.write(', '.join(sep[1::2]))
             wp.write('\n')
     return outputname
 
 
 def run_eval(sys_truth, test_num):
-    task = {'1':'SIGHAN15_CSC_DryTruth.txt','2':'SIGHAN15_CSC_TestTruth.txt'}
+    task = {'1': 'SIGHAN15_CSC_DryTruth.txt',
+            '2': 'SIGHAN15_CSC_TestTruth.txt'}
     judge = 'sighan15csc.jar'
 
-    detail_path = 'de_'+os.path.splitext(sys_truth)[0]+'.txt'
+    sys_filename = os.path.splitext(sys_truth)[0]
+    sys_token = sys_filename[sys_filename.find('_')+1:]
+    detail_path = 'de_' + sys_token + '.txt'
     cmd_eval = 'java -jar %s -s %s -i %s -t %s -o %s'\
-            %(judge, test_num, sys_truth, task[test_num] , detail_path)
-    #  print(cmd_eval)
+        % (judge, test_num, sys_truth, task[test_num], detail_path)
+    # print(cmd_eval)
     if os.path.exists(judge) & os.path.exists(task[test_num]):
         os.system(cmd_eval)
-    print('Output file = '+detail_path)
-        
+    print('Output file = ' + detail_path)
+
+    return (detail_path, sys_token)
+
+def getVal(filename, token):
+    output = list()
+    with open(filename, 'r', encoding='utf8') as fp:
+        for idx,line in enumerate(fp,1):
+            # False Positive
+            if idx == 7:
+                output.append(line.split()[4])
+            elif idx >= 10:
+                tmp = line.split()
+                if len(tmp)==4:
+                    output.append(tmp[2])
+            elif idx > 22:
+                break
+
+    return output
+    # with open('log_{}.txt'.format(token), 'w', encoding='utf8') as fp:
+    #     fp.write('\t'.join(output))
+
+    # print('Logfile = log_{}.txt'.format(token))
+    # return 'log_{}.txt'.format(token)
+            
 
 if __name__ == "__main__":
-    if len(sys.argv)>=3:
-        sys_truth = sys.argv[1]
-        test_num = sys.argv[2]
+    if len(sys.argv) >= 3:
+        TEST_NUM = sys.argv[1]
         
-        if not os.path.exists(sys_truth):
-            print('File:{} not exists.'.format(sys_truth))
-            sys.exit(0)
+        SYS_TRUTHS = [filename for filename in sys.argv[2:]]
 
-        if len(sys.argv)==4:
-            print('Doing remove character...')
-            sys_truth = rmchar(sys_truth)
-        run_eval(sys_truth, test_num)
+        log_files = list()
+        for sys_file in SYS_TRUTHS:
+            if not os.path.exists(sys_file):
+                print('File:{} not exists.'.format(sys_file))
+                sys.exit(0)
+
+            (detail_filename, token) = run_eval(sys_file, TEST_NUM)
+            log_files.append(getVal(detail_filename, token))
+        
+        
+        
+        t = datetime.now()
+        log_token = '{:02}{:02}_{:02}{:02}{:02}'.format(t.month,t.day,t.hour,t.minute,t.second)
+        with open('log_{}'.format(log_token), 'w', encoding='utf8') as fp:
+            print('Log file = log_{}'.format(log_token))            
+            for batch in log_files:
+                fp.write('\t'.join(batch))
+                fp.write('\n')
+            
+
+
+
+
+
+
     else:
-        print('Usage: python3 **.py sys_truth_filename test_num (rm char)')
+        print('Usage: python3 **.py sys_truth_filename test_num ')
         print('test_num: Dry=1 Test=2')
         sys.exit(0)
-
-            
-        
